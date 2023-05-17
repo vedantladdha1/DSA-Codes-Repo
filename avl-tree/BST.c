122a099d1bdf23dcba47c8b637e8a823fa51d855
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2023-05-17 16:44:33 +0000

    completed the binary

--- avl-tree/BST.c
+++ avl-tree/BST.c
@@ -34,9 +34,9 @@ Node* insert(Node* node, int key)
 
 //search
 Node* search(Node *node ,  int key){
-    if(key == node->key || node == NULL)
+    if(key == node->key)
         return node; 
-    
+     
     if(key < node->key)
          return search(node->left , key);
     else if(key > node->key)
@@ -53,13 +53,61 @@ void inorder(Node* root)
     }
 }
 
+Node *minValueNode(Node *node){
+    Node* current = node ; 
+    
+    while(current&& current->left != NULL){
+        current = current->left ; 
+    }
+
+    return current ; 
+}
+
+
+//delete function
+Node *deleteNode(int key , Node* node){
+    if (node == NULL)
+        return node ; 
+    
+    if(key < node->key)
+        node->left  = deleteNode(key , node->left); 
+    else if(key > node->key)
+        node->right = deleteNode(key , node->right); 
+    
+    else{
+        // when the node has a single child or no child
+        if(node->left == NULL){
+            Node *temp  = node->right;
+            free(node); 
+            return temp;
+        }
+        else if (node->right == NULL){
+            Node *temp = node->left ; 
+            free(node);
+            return temp; 
+        }
+         Node *temp = minValueNode(node->right); 
+        node->key = temp->key;
+
+        node->right = deleteNode(temp->key , node->right);
+    }
+   
+   return node ; 
+
+}
+
 //Driver code 
 void main(){
     Node *root = NULL;
     root = insert(root , 69); 
+    insert(root ,  79);
     insert(root ,  100); 
     insert(root ,  71); 
     insert(root ,  89); 
     insert(root ,  90); 
+    deleteNode(71 ,root);
+    deleteNode(100 , root);
+
+    inorder(root);
 
 }
